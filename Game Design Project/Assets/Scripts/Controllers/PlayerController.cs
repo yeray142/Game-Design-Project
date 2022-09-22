dc26566a7775be4d39ceead174c84e4d145062d8
Fran Martín <franhm@uma.es>
2022-09-22 10:01:17 +0900

    Movement: First version finished

--- Game Design Project/Assets/Scripts/Controllers/PlayerController.cs
+++ Game Design Project/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,30 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour 
 {
-    public CharacterController controller;
-    private Vector3 direction;
-    public float speed = 8;
-    public float gravity = 9.8f;
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed; // 10
+    [SerializeField]
+    private float _gravity; // 0.11
+    [SerializeField]
+    private float _jumpHeight; // 18
+
+    private float _yVelocity;
+    private bool _canDoubleJump;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float hInput = Input.GetAxis("Horizontal");
-        direction.x = hInput * speed;
+        Vector3 direction = new Vector3(hInput, 0, 0);
+        Vector3 velocity = direction * _speed;
 
-        float vInput = Input.GetAxis("Vertical");
         
-        //direction.y = hInput * speed;
+        if (_controller.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                _yVelocity = _jumpHeight;
+                _canDoubleJump = true;
+            }
+        } else
+        {
+            if(_canDoubleJump && Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                _yVelocity += _jumpHeight;
+                _canDoubleJump = false;
+            }
+            _yVelocity -= _gravity;
+        }
 
-        controller.Move(direction * Time.deltaTime);
+        velocity.y = _yVelocity;
+
+        _controller.Move(velocity * Time.deltaTime);
     }
 }
