78b2349246be87a4c38d45989fc7c6244888a675
yeray142 <yeraycorderocarrasco@gmail.com>
2022-11-24 12:32:43 +0900

    Update StateManager.cs

--- Game Design Project/Assets/Scripts/StateManager.cs
+++ Game Design Project/Assets/Scripts/StateManager.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Diagnostics;
 using Unity.XR.OpenVR;
 using UnityEngine;
 using UnityEngine.UI;
@@ -41,11 +42,93 @@ public class StateManager : MonoBehaviour
     {
        // handleDC = GetComponent<HandleDamageColliders>();
        // handleAnim = GetComponent<HandleAnimations>();
+       // handleMovement = GetComponent<HandleMovement>();
+
+        sRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        sRenderer.flipX = lookRight;
+
+        onGround = isOnGround();
+
+        if (healthSlider != null)
+            healthSlider.value = health * 0.01f;
+
+        if (health <= 0)
+        {
+            /*
+            if (LevelManager.GetInstance().countdown)
+            {
+                LevelManager.GetInstance().EndTurnFunction();
+                handleAnim.anim.Play("Dead");
+            }
+            */
+        }
+    }
+
+    bool isOnGround()
+    {
+        bool ret = false;
+
+        LayerMask layer = ~(1 << gameObject.layer | 1 << 3);
+        ret = Physics2D.Raycast(transform.position, -Vector2.up, 0.1f, layer);
+
+        return ret;
+    }
+
+    public void ResetStateInputs()
+    {
+        horizontal = 0;
+        vertical = 0;
+        attack1 = false;
+        attack2 = false;
+        attack3 = false;
+        crouch = false;
+        gettingHit = false;
+        currentlyAttacking = false;
+        dontMove = false;
+    }
+
+    public void CloseMovementCollider(int i)
+    {
+        movementColliders[i].SetActive(false);
+    }
+
+    public void OpenMovementCollider(int i)
+    {
+        movementColliders[i].SetActive(true);
+    }
+
+    /*
+    public void TakeDamage(int damage, HandleDamageColliders.DamageType damageType)
+    {
+        if (!gettingHit)
+        {
+            switch (damageType)
+            {
+                case HandleDamageColliders.DamageType.ligth:
+                    StartCoroutine(CloseInmortality(0.3f));
+                    break;
+                case HandleDamageColliders.DamageType.heavy:
+                    handleMovement.AddVelocityOnCharacter(
+                        ((!lookRight) ? Vector3.right * 1 : Vector3.right * -1) + Vector3.up,
+                        0.5f
+                        );
+                    StartCoroutine(CloseInmortality(1));
+                    break;
+            }
+
+            health -= damage;
+            gettingHit = true;
+        }
+    }
+    */
+
+    IEnumerator CloseInmortality(float timer)
+    {
+        yield return new WaitForSeconds(timer);
+        gettingHit = false;
     }
 }
